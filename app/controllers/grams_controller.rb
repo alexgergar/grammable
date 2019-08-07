826a0f35aa8aefb3a63bc40864544a8270999555
Alex Gergar <alexandra.gergar@gmail.com>
2019-08-07 20:58:58 +0000

    L7 - added and tested gram model and controller for new and create methods

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,9 +1,24 @@
 class GramsController < ApplicationController
+
   def index
     
   end
 
   def new
     @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+    if @gram.invalid?
+        flash[:error] = '<strong>No Message</strong> - the data you entered is invalid!'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
   end
 end
