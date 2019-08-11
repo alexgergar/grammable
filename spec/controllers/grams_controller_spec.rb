41f224033e0e239c7bbafee2b1081441853df7f5
Alex Gergar <alexandra.gergar@gmail.com>
2019-08-11 18:53:40 +0000

    L13 added edit and update to grams with testing

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -68,6 +68,42 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+  describe "grams#edit action" do
+    it "should successfully show the edit form if the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 4040 error message if the gram is not found" do
+      get :show, params: { id: 'ChillyWilly' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "grams#update action" do
+    it "should all users to successfully update grams" do
+      gram = FactoryBot.create(:gram, message: "Initial Value")
+      patch :update, params: { id: gram.id, gram: { message: "Changed"} }
+      expect(response).to redirect_to root_path
+      gram.reload
+      expect(gram.message).to eq "Changed"
+    end
+
+    it "should have http 404 error if the gram can't be found" do
+      patch :update, params: { id: "yoloswag", gram: { message: "Changed"} }
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should show a render the edit form if there are validation errors and show the unprocessale_entity" do
+      gram = FactoryBot.create(:gram, message: "Initial Value")
+      patch :update, params: { id: gram.id, gram: { message: ""} }
+      expect(response).to have_http_status(:unprocessable_entity)
+      gram.reload
+      expect(gram.message).to eq "Initial Value"
+    end
+  end
+
   
 end
 
