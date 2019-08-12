5c07cfab0f23b1aad090d698cf85bf0282734b25
Alex Gergar <alexandra.gergar@gmail.com>
2019-08-12 01:45:33 +0000

    L17 Adding validation for images with factory bot

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,7 +1,8 @@
 class Gram < ApplicationRecord
-   mount_uploader :picture, PictureUploader
+  mount_uploader :picture, PictureUploader
   
   belongs_to :user
-
+  
+  validates :picture, presence: true
   validates :message, presence: true
 end
